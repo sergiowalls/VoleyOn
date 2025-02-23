c0d2bab281b2dbd4f8371c795c1e740d73a2285b
Sergio Paredes <sparedess@uoc.edu>
2025-02-23 18:59:50 +0100

    Set environment variables for backend

--- backend/voleyon/settings.py
+++ backend/voleyon/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rd+2ejb**&%vhj)#psh*2$*nxr9wfu7qwy!ujp1-h83ld#+3gz'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-rd+2ejb**&%vhj)#psh*2$*nxr9wfu7qwy!ujp1-h83ld#+3gz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
