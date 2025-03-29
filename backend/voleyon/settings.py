2d690d1f4341df0330a6ad68989469bac06cbb03
Sergio Paredes <sparedess@uoc.edu>
2025-03-29 13:03:09 +0100

    Add VoleyOn domain to allowed CORS

--- backend/voleyon/settings.py
+++ backend/voleyon/settings.py
@@ -120,7 +120,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOWED_ORIGINS = [ 'http://localhost:5173', ]
+CORS_ALLOWED_ORIGINS = [ 'https://voleyon.com', 'http://localhost:5173', ]
 
 
 # Static files (CSS, JavaScript, Images)
