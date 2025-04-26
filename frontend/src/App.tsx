41a27afea51e74227ac0964c86070864e85f7b95
Sergio Paredes <sparedess@uoc.edu>
2025-04-26 18:27:10 +0200

    Set localization provider to Spanish

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import 'dayjs/locale/es';
 
 import './App.css'
 import Tournaments from "./Tournaments.tsx";
 
 function App() {
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es">
             <Tournaments></Tournaments>
         </LocalizationProvider>
     )
